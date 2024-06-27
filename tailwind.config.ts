6280cf1c0586cb5054d6a1ef2f1500d5330dc5df
Konadu-Akwasi-Akuoko <konaduakwasiakuoko@gmail.com>
2024-06-27 07:59:25 +0000

    fix: keeping the layout to default

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,23 +1,15 @@
 import { nextui } from "@nextui-org/react";
 import type { Config } from "tailwindcss";
 
-
-
-
-
-
-
-
 const config: Config = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {
-
-    },
+    extend: {},
     screens: {
       xs: "320px",
       sm: "640px",
@@ -28,38 +20,25 @@ const config: Config = {
     },
   },
 
-
-  plugins: [nextui({
-    defaultTheme: "dark",
-    addCommonColors: true, // override common colors (e.g. "blue", "green", "pink").
-    defaultExtendTheme: "light",
-    themes: {
-      light: {
-
-        layout: {}, // light theme layout tokens
-        colors: {
-
-        } // light theme colors
+  plugins: [
+    nextui({
+      defaultTheme: "dark",
+      addCommonColors: true, // override common colors (e.g. "blue", "green", "pink").
+      defaultExtendTheme: "light",
+      themes: {
+        light: {
+          // layout: {}, // light theme layout tokens
+          colors: {}, // light theme colors
+        },
+        dark: {
+          // layout: {}, // dark theme layout tokens
+          colors: {
+            primary: {},
+            secondary: {},
+          }, // dark theme colors
+        },
       },
-      dark: {
-        layout: {}, // dark theme layout tokens
-        colors: {
-
-          primary: {
-
-          },
-          secondary: {
-
-
-          }
-
-
-        } // dark theme colors
-      },
-
-    }
-  }),
-
+    }),
   ],
 };
 export default config;
