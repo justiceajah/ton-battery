eb1ecd135dc3d52b50cf0b7b475579fa2864527e
Konadu-Akwasi-Akuoko <konaduakwasiakuoko@gmail.com>
2024-06-28 02:51:30 +0000

    feat: installed tailwindcss automatic class sorting

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,11 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import Providers from "./providers";
-
+import { Header } from "@/components/Header";
 
 export const metadata: Metadata = {
   title: "Bone Cracking",
-  description: "Task page"
+  description: "Task page",
 };
 
 export default function RootLayout({
@@ -15,9 +15,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-
       <body className="font-audiNormal">
         <Providers>
+          <Header />
           <main className="z-[20]">{children}</main>
           <footer></footer>
         </Providers>
---
+++ src/components/Header.tsx
@@ -0,0 +1,16 @@
+export function Header() {
+  return (
+    <header className="flex w-full flex-row items-center">
+      <nav className="w-full">
+        <ul className="flex w-full flex-row items-center">
+          <li className="w-fit">FAQ</li>
+          <li className="w-fit">Support</li>
+          <li className="w-fit">Documentation</li>
+          <li className="w-fit">Bug Bounty</li>
+          <li className="w-fit">Github</li>
+          <li className="w-fit">Tonkeeper Battery</li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
